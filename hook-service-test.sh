589bbaf3efdf8b2388abf565c0b92e54ed6a1d44
Víctor Vela López <36082719+VictorVelaLopez@users.noreply.github.com>
2020-07-07 20:03:04 +0200

    Create hook-service-test.sh

---
+++ hook-service-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+while read linea ; do
+
+instance="`echo $linea |awk -F "," '{print $1}'`"
+name="`echo $linea |awk -F "," '{print $2}'`"
+
+ssh root@"$instance" systemctl start "$name" >> hooh-service-test-output.log
+
+done < service_notify_test.log
