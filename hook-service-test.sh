8f023783a96929a40dd3f0e1bb043bc14d579404
Víctor Vela López <36082719+VictorVelaLopez@users.noreply.github.com>
2020-07-07 20:51:47 +0200

    Update hook-service-test.sh

--- hook-service-test.sh
+++ hook-service-test.sh
@@ -5,6 +5,6 @@ while read linea ; do
 instance="`echo $linea |awk -F "," '{print $1}'`"
 name="`echo $linea |awk -F "," '{print $2}'`"
 
-ssh root@"$instance" systemctl start "$name" >> hooh-service-test-output.log
+ssh root@"$instance" systemctl start "$name"
 
 done < service-notify-test.tmp
